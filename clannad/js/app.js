e7f8b7b46f2f865483b3f86660ed404a0a32c790
magazila <msabarac@gmail.com>
2026-08-11 17:18:07 +0300

    Update app.js

--- clannad/js/app.js
+++ clannad/js/app.js
@@ -1,42 +1,49 @@
-$(function () {
-    let currentOffset = 0;
-    const step = 920;
-    const maxOffset = -8280;
+let header = document.querySelector('.header')
+let open__menu = document.querySelector('.open__menu')
+let header__link = document.querySelectorAll('.header__link')
+let all__card = document.querySelectorAll('.chara__card')
+let count__card = all__card.length
+let step = 1135
+let now__step = 0
+let max__scroll = -(step * (count__card - 1))
 
-    function updateButtons() {
-        if (currentOffset === 0) {$('.chara__container-left').hide();} 
-        else {$('.chara__container-left').show();}
-        if (currentOffset <= maxOffset) {$('.chara__container-rigth').hide();} 
-        else {$('.chara__container-rigth').show();}
+let rigth__arrow = document.querySelector('.chara__container-rigth')
+let left__arrow = document.querySelector('.chara__container-left')
+let chara__container = document.querySelector('.chara__container')
+
+// console.log(count__card)
+// console.log(max__scroll)
+// console.log('right arrow:', rigth__arrow);
+// console.log('left arrow:', left__arrow);
+// console.log('container:', chara__container);
+// console.log('Количество карточек:', count__card);
+// console.log('Максимальный сдвиг:', max__scroll);
+// console.log('Шаг:', step);
+
+open__menu.addEventListener('click', function () {
+    header.classList.toggle('openned__menu')
+})
+
+for (let link of header__link)
+    link.addEventListener('click', function () {
+        header.classList.remove('openned__menu')
+    });
+
+rigth__arrow.addEventListener('click', function () {
+
+    if (now__step - step >= max__scroll) {
+        now__step -= step;
+        chara__container.style.transform = `translateX(${now__step}px)`;
     }
-    $('.open__menu').on('click', function () {
-        $('.header').toggleClass('openned__menu');
-    });
-
-    $('.header__link').on('click', function () {
-        $('.header').toggleClass('openned__menu');
-    });
-    $('.open__menu').html('<i class="fa-solid fa-ellipsis-vertical"></i>');
-
-    updateButtons();
-
-    $('.chara__container-rigth').on('click', function () {
-        if (currentOffset > maxOffset) {
-            currentOffset -= step;
-            $('.chara__container').css({
-                'transform': `translateX(${currentOffset}px)`
-            });
-            updateButtons();
-        }
-    });
-
-    $('.chara__container-left').on('click', function () {
-        if (currentOffset < 0) {
-            currentOffset += step;
-            $('.chara__container').css({
-                'transform': `translateX(${currentOffset}px)`
-            });
-            updateButtons();
-        }
-    });
 });
+
+left__arrow.addEventListener('click', function () {
+    if (now__step + step <= 0) {
+        now__step += step;
+        chara__container.style.transform = `translateX(${now__step}px)`;
+    }
+});
+
+
+
+
