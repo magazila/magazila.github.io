b065c0a3c4be386324814ed2338cb0395a8d7871
magazila <msabarac@gmail.com>
2026-08-15 19:24:26 +0300

    картчоки скрол,

--- wending/js/app.js
+++ wending/js/app.js
@@ -8,10 +8,11 @@ const body = document.querySelector('body')
 const theme = document.querySelector('.theme')
 
 
+
+
 burger.addEventListener('click', function () {
     menu.classList.toggle('open')
 })
-
 for (const link of menuLink) {
     link.addEventListener('click', function () {
         menu.classList.remove('open')
@@ -19,15 +20,42 @@ for (const link of menuLink) {
 }
 
 landscapeClose.addEventListener('click', function () {
-
     landscape.style.display = 'none';
-
-
 })
+    // theme.addEventListener('click', function () {
+    //     body.classList.toggle('dark')
+    // })
 
-theme.addEventListener('click', function () {
-    body.classList.toggle('dark')
-})
-console.log(body);
-console.log(theme);
+
+const massFeaturedCard = document.querySelectorAll('.featured__card')
+const countCard = massFeaturedCard.length
+let cardWidht = document.querySelector('.featured__card').clientWidth
+let positionCard = 0
+const step = cardWidht + 20
+const leftArrow = document.querySelectorAll('.featured__card-left-arrow')
+const rightArrow = document.querySelectorAll('.featured__card-right-arrow')
+const containerCard = document.querySelector('.featured__card-wrapper')
+
+
+
+
+
+for (let right of rightArrow) {
+    right.addEventListener('click', function(){
+        positionCard = positionCard - step
+        containerCard.style.transform = `translateX(${positionCard}px)`
+    })
+}
+for (let left of leftArrow) {
+    left.addEventListener('click', function(){
+        positionCard = positionCard + step
+        containerCard.style.transform = `translateX(${positionCard}px)`
+    })
+}
+
+
+console.log(countCard)
+console.log(cardWidht)
+console.log(step)
+
 
